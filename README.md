af0b46a20f3b831eb657e15cfde0cc396387fe5c
Christian Himpe <christian.himpe@wwu.de>
2022-11-13 21:51:37 +0100

    init

---
+++ README.md
@@ -0,0 +1,263 @@
+arcadedb (v1)
+=============
+
+`arcadedb` is a CHICKEN Scheme egg module providing a driver or REPL
+for the [**ArcadeDB**](https://arcadedb.com) database.
+
+## About **ArcadeDB**
+
+**ArcadeDB** is a multi-model NoSQL database providing graph and document models,
+while supporting a wide range of data query languages, such as:
+
+* [SQL](https://docs.arcadedb.com/#_sql) (dialect),
+* [Cypher](https://opencypher.org/resources/),
+* [Gremlin](https://tinkerpop.apache.org/docs/current/),
+* [GraphQL](https://graphql.org/),
+* [Mongo](https://www.mongodb.com/docs/manual/)
+
+as well as providing a HTTP/JSON/REST API.
+
+### SQL
+
+The native query language of **ArcadeDB** is a dialect of SQL, closely related to
+_OrientDB_'s OSQL, which supports the [SQL command categories](https://www.geeksforgeeks.org/sql-ddl-dql-dml-dcl-tcl-commands/):
+
+* **DDL** - Data Definition Language, via `CREATE`, `DROP`, `ALTER`, `TRUNCATE` of `TYPE`s
+* **DQL** - Data Query Language, via `SELECT`, `TRAVERSE`, `MATCH`
+* **DML** - Data Manipulation Language, via `INSERT`, `UPDATE`, `DROP`, `EXPLAIN`, `PROFILE`
+
+for the remaining categories holds:
+
+* **DCL** - Data Control Language, does not apply due to only [server level users](https://docs.arcadedb.com/#Security)
+* **TCL** - Transaction Control Language, via [HTTP REST endpoints](https://docs.arcadedb.com/#HTTP-API)
+
+## About `arcadedb`
+
+The `arcadedb` module implements a driver and console for **ArcadeDB** in
+CHICKEN Scheme with the functionality:
+
+* [Server information](#server-information)
+* [Database management](#database-management)
+* [Database interaction](#database-interaction)
+* [Database macros](#database-macros)
+
+### Dependencies
+
+Naturally, `arcadedb` requires a remote or local **ArcadeDB** server:
+
+* [ArcadeDB](https://github.com/ArcadeData/arcadedb/releases/latest)
+
+which in turn requires a Java distribution, i.e. OpenJDK 11.
+A local server setup is described below.
+Furthermore, the `arcadedb` module requires `curl` for the HTTP requests:
+
+* [curl](http://curl.se)
+
+and imports the `uri-common` egg to url-encode strings,
+as well as the `medea` egg to decode JSON:
+
+* [uri-common](https://wiki.call-cc.org/eggref/5/uri-common)
+* [medea](https://wiki.call-cc.org/eggref/5/medea)
+
+## Local Server Setup
+
+A local **ArcadeDB** server can be set up via [install](#install) or [container](#container).
+
+### Install
+
+1. Download package: [ArcadeDB **package**](https://github.com/ArcadeData/arcadedb/releases/latest)
+2. Extract package: `tar -xf arcadedb-latest.tar.gz`
+3. Start server: `ARCADEDB_HOME=/path/to/arcadedb/ bin/server.sh -Darcadedb.server.rootPassword=mypassword &` 
+4. Exit server: ``kill `cat bin/arcade.pid` ``
+
+### Container
+
+0. Install [Docker](https://www.docker.com/)
+1. Download container: `docker pull arcadedata/arcadedb`
+2. Start container: `docker run --rm -d -p 2480:2480 -e JAVA_OPTS="-Darcadedb.server.rootPassword=mypassword --name arcadedb0 arcadedata/arcadedb`
+3. Stop container: `docker stop arcadedb0`
+
+## Procedures
+
+### Help Message
+
+#### a-help
+```
+(a-help)
+```
+Returns **void**, prints help about using the `arcadedb` module.
+
+### Server Connection
+
+#### a-connect
+```
+(a-connect user pass host . port)
+```
+Returns **boolean** answering if connection to server using **string**s `user`,
+`pass`, `host`, and optionally **number** `port` was succesful;
+returns `#f` if a server error occurs or no response is received.
+
+### Server Information
+
+#### a-status
+```
+(a-status)
+```
+Returns **list** holding the cluster configuration of the server;
+returns empty **list** `'()` if no replicas are configured;
+returns `#f` if a server error occurs or no response is received.
+
+#### a-healthy?
+```
+(a-healthy?)
+```
+Returns **boolean** answering if server is ready.
+
+#### a-list
+```
+(a-list)
+```
+Returns **list** of **symbol**s holding available databases of the server;
+returns `#f` if a server error occurs or no response is received.
+
+#### a-version
+```
+(a-version)
+```
+Returns **string** version number of the server;
+returns `#f` if a server error occurs or no response is received.
+
+### Database Management
+
+#### a-exist?
+```
+(a-exist? db)
+```
+Returns **boolean** answering if database **symbol** `db` exists of the server.
+
+#### a-create
+```
+(a-create db)
+```
+Returns **boolean** that is true if creating new database **symbol** `db` on the server was succesful;
+returns `#f` if a server error occurs or no response is received.
+
+#### a-open?
+```
+(a-open? db)
+```
+Returns **boolean** answering if database **symbol** `db` is open on the server.
+
+#### a-open
+```
+(a-open db)
+```
+Returns **boolean** that is true if opening database **symbol** `db` on the server was succesful;
+returns `#f` if a server error occurs or no response is received.
+
+#### a-close
+```
+(a-close db)
+```
+Returns **boolean** that is true if closing database **symbol** `db` on the server was succesful;
+returns `#f` if a server error occurs or no response is received.
+
+#### a-drop
+```
+(a-drop db)
+```
+Returns **boolean** that is true if deleting database **symbol** `db` on the server was successful;
+returns `#f` if a server error occurs or no response is received.
+
+### Database Interaction
+
+#### a-query
+```
+(a-query db lang query)
+```
+Returns **list** holding the result of **string** `query` in language **symbol** `lang` of database **symbol** `db`;
+returns `#f` if a server error occurs or no response is received.
+
+#### a-command
+```
+(a-command db lang cmd)
+```
+Returns **list** holding the result of **string** `cmd` in language **symbol** `lang` to database **symbol** `db`;
+returns `#f` if a server error occurs or no response is received.
+
+#### a-script
+```
+(a-script db path)
+```
+Returns **list** holding the result of the last statement of SQL script in **string** `path` to database **symbol** `db`;
+returns `#f` if a server error occurs or no response is received.
+
+A SQL script file has to have the file extension `.sql`.
+
+### Database Macros
+
+#### a-import
+```
+(a-import db url)
+```
+**boolean** that is true if importing from **string** `url` into database **symbol** `db` on the server was successful;
+returns `#f` if a server error occurs or no response is received.
+
+This function can be a minimalistic ETL (Extract-Transform-Load) tool:
+If one needs to import data from another database with a HTTP API
+and the query can be encoded ([as for **ArcadeDB**](https://docs.arcadedb.com/#HTTP-API)) in the URL,
+the extraction and transformation is performed in the remote query,
+while the loading corresponds to the import of the query result.
+The supported formats are [OrientDB, Neo4J, GraphML, GraphSON, XML, CSV, JSON, RDF](https://docs.arcadedb.com/#Importer).
+
+#### a-describe
+```
+(a-describe db)
+```
+Returns **alist** of type descriptions for database **symbol** `db`;
+returns `#f` if a server error occurs or no response is received.
+
+This function emulates the SQL `DESCRIBE` statement.
+
+#### a-load
+```
+(a-load db path type)
+```
+Returns **boolean** that is true if loading _JSON_ file at **string** `path`
+into database **symbol** `db` as **symbol** `type` on the server was successful;
+returns `#f` if a server error occurs or no response is received.
+
+#### a-backup
+```
+(a-backup db)
+```
+Returns **boolean** that is true if backing-up database **symbol** `db` on the server was successful.
+
+#### a-check
+```
+(a-check db)
+(a-check db fix?)
+```
+Returns **list**-of-**alist**s integrity check report, attempts to fix if true **boolean** `fix?` is passed.
+returns `#f` if a server error occurs or no response is received.
+
+#### a-comment
+```
+(a-comment db)
+(a-comment db msg)
+```
+Returns **string** current database comment of database **symbol** `db`, if `msg` is not passed;
+returns `#t` if **string** `msg` was set as comment for database **symbol** `db` on the server succesfully;
+returns `#f` if no comment is set, a server error occurs or no response is received.
+
+This function emulates the SQL `COMMENT ON DATABASE` statement,
+by creating a type `D` and upserting or reading the first `comment` property.
+
+## Changelog
+
+* `1` [Initial Release](https://github.com/gramian/chicken-arcadedb) (2022-??-??)
+
+## License
+
+Copyright (c) 2022 _Christian Himpe_ under [zlib-acknowledgement](https://spdx.org/licenses/zlib-acknowledgement.html) license.
+
---
+++ arcadedb.scm
@@ -0,0 +1,235 @@
+;;;; ArcadeDB CHICKEN Scheme Module
+
+;;@project: chicken-arcadedb
+;;@version: 1 (2022-??-??)
+;;@authors: Christian Himpe (0000-0003-2194-6754)
+;;@license: zlib-acknowledgement (spdx.org/licenses/zlib-acknowledgement.html)
+;;@summary: A database driver for ArcadeDB 
+
+(module arcadedb
+
+  (a-help
+
+   a-connect
+
+   a-status
+   a-healthy?
+   a-list
+   a-version
+
+   a-exist?
+   a-create
+   a-open?
+   a-open
+   a-close
+   a-drop
+
+   a-query
+   a-command
+   a-script
+
+   a-import
+   a-describe
+   a-load
+   a-backup
+   a-check
+   a-comment
+  )
+
+  (import scheme (chicken base) (chicken io) (chicken string) (chicken process) (chicken pathname) uri-common medea)
+
+(print "\n; arcadedb: call `(a-help)` for a procedure overview.\n")
+
+(define-constant version 1)
+
+(define server (make-parameter #f))
+
+;; Utility Functions ###########################################################
+
+(define (ok? resp)
+  (and resp (string-ci=? "ok" (alist-ref 'result resp))))
+
+(define (result resp)
+  (and resp (vector->list (alist-ref 'result resp))))
+
+(define (supported? lang)
+  (memq lang '(sql cypher gremlin graphql mongo)))
+
+(define (http method endpoint #!key (body '()) (session #f) (notify #t))
+  (let* [(curl "curl -s")
+         (type (case method ['get  " -X GET"]
+                            ['head " -X POST -I"]
+                            ['post " -X POST"]))
+         (head " -H Content-Type: application/json")
+         (sess (if session (string-append " -H " session) "")) 
+         (data (if (null? body) "" (string-append " -d '" (json->string body) "'")))
+         (resp (with-input-from-pipe (apply string-append curl type head sess data " " (server) endpoint)
+                                     (if (eqv? method 'head) read-lines read-json)))]
+           (cond ((not resp)
+                    (begin (print "No Server Response!") #f))
+                 ((and ((list-of? pair?) resp) (alist-ref 'error resp))
+                    (begin (when notify (print "Server Error: " (alist-ref 'detail resp))) #f))
+                 (else resp))))
+
+;;; Help Message ###############################################################
+
+;;@returns: **void**, prints help on using the arcadedb module.
+(define (a-help)
+  (print "\n"
+         " ╔══════════════════╗\n"
+         " ║ 🐔-ArcadeDB (v" (number->string version) ") ║\n"
+         " ╚══════════════════╝\n"
+         "\n"
+         " A CHICKEN Scheme database driver module egg for ArcadeDB (https://arcadedb.com)\n"
+         "\n"
+         " (a-help)                          - Display this message\n"
+         "\n"
+         " (a-connect user pass host . port) - Connect remote server\n"
+         "\n"
+         " (a-status)                        - Cluster configuration\n"
+         " (a-healthy?)                      - Is server alive?\n"
+         " (a-list)                          - List databases\n"
+         " (a-version)                       - Server version\n"
+         "\n"
+         " (a-exist? db)                     - Does database exist?\n"
+         " (a-create db)                     - Create database\n"
+         " (a-open? db)                      - Is database open?\n"
+         " (a-open db)                       - Open database\n"
+         " (a-close db)                      - Close database\n"
+         " (a-drop db)                       - Delete database\n"
+         "\n"
+         " (a-query db lang query)           - Database query\n"
+         " (a-command db lang cmd)           - Database command\n"
+         " (a-script db path)                - Database script\n"
+         "\n"
+         " (a-import db url)                 - Import database\n"
+         " (a-describe db)                   - List types\n"
+         " (a-load db path type)             - Load document\n"
+         " (a-backup db)                     - Backup database\n"
+         " (a-check db [fix?])               - Check database\n"
+         " (a-comment db [msg])              - Database comment\n"
+         "\n"
+         " For more info see: http://call-cc.org/egg/arcadedb\n"))
+
+;;; Server Connection ##########################################################
+
+;;@returns: **boolean** answering if connection to server using **string**s `user`, `pass`, `host`, and optionally **number** `port` was succesful.
+(define (a-connect user pass host . port)
+  (assert (and (string? user) (string? pass) (string? host)))
+  (server (string-append "http://" user ":" pass "@" host ":" (number->string (optional port 2480)) "/api/v1/"))
+  (if (a-healthy?) '((arcadedb . "Welcome")) #f))
+
+;;; Server Information #########################################################
+
+;;@returns: **list** holding cluster configuration of the server, or #f.
+(define (a-status)
+  (assert (server))
+  (http 'get '("server")))
+
+;;@returns: **boolean** answering if the server is ready.
+(define (a-healthy?)
+  (not (not (a-status))))
+
+;;@returns: **list** of **symbols** holding available databases of the server, of #f.
+(define (a-list)
+  (assert (server))
+  (map string->symbol (result (http 'get '("databases")))))
+
+;;@returns: **string** version number of the server, or #f.
+(define (a-version)
+  (assert (server))
+  (let [(resp (http 'get '("databases")))]
+    (and resp (alist-ref 'version resp))))
+
+;;; Database Management ########################################################
+
+;;@returns: **boolean** answering if database **symbol** `db` exists of the server.
+(define (a-exist? db)
+  (not (not (http 'post `("open/" ,(symbol->string db)) notify: #f))))
+
+;;@returns: **boolean** that is true if creating new database **symbol** `db` on the server was succesful.
+(define (a-create db)
+  (assert (server))
+  (ok? (http 'post `("create/" ,(symbol->string db)))))
+
+;;@returns: **boolean** answering if database **symbol** `db` is open on the server.
+(define (a-open? db)
+  (assert (server))
+  (not (not (http 'get `("query/" ,(symbol->string db) "/sql/SELECT%20true") notify: #f))))
+
+;;@returns: **boolean** that is true if opening database **symbol** `db` on the server was succesful.
+(define (a-open db)
+  (assert (server))
+  (ok? (http 'post `("open/" ,(symbol->string db)))))
+
+;;@returns: **boolean** that is true if closing database **symbol** `db` on the server was succesful.
+(define (a-close db)
+  (assert (server))
+  (ok? (http 'post `("close/" ,(symbol->string db)))))
+
+;;@returns: **boolean** that is true if deleting database **symbol** `db` on the server was successful.
+(define (a-drop db)
+  (assert (server))
+  (ok? (http 'post `("drop/" ,(symbol->string db)))))
+
+;;; Database Interactions ######################################################
+
+;;@returns: **list** holding the result of **string** `query` in language **symbol** `lang` of database **symbol** `db`.
+(define (a-query db lang query)
+  (assert (and (supported? lang) (server) (symbol? db) (string? query)))
+  (result (http 'get `("query/" ,(symbol->string db) "/" ,(symbol->string lang) "/" ,(uri-encode-string query)))))
+
+;;@returns: **list** holding the result of **string** `cmd` in language **symbol** `lang` to database **symbol** `db`.
+(define (a-command db lang cmd)
+  (assert (and (supported? lang) (server) (symbol? db) (string? cmd)))
+  (result (http 'post `("command/" ,(symbol->string db)) body: `((language . ,(symbol->string lang))
+                                                                 (command . ,cmd)))))
+
+;;@returns: **list** holding the result of the last statement of _SQL_ script in **string** `path` to database **symbol** `db`.
+(define (a-script db path)
+  (assert (and (string? path) (string=? "sql" (pathname-extension path)) (symbol? db) (server)))
+  (result (http 'post `("command/" ,(symbol->string db)) body: `((language . "sqlscript")
+                                                                 (command . ,(read-string #f (open-input-file path)))))))
+
+;;; Database Macros ############################################################
+
+;;@returns: **boolean** that is true if importing from **string** `url` into database **symbol** `db` as **symbol** `type` on the server was successful.
+(define (a-import db url)
+  (let [(res (a-command db 'sql (string-append "IMPORT DATABASE " url)))]
+    (and res (not (null? res)) (ok? (car res)))))  
+
+;;@returns: **alist** of type descriptions for database **symbol** `db`.
+(define (a-describe db)
+  (a-query db 'sql "SELECT FROM schema:types"))
+
+;;@returns: **boolean** that is true if loading _JSON_ file at **string** `path` into database **symbol** `db` as **symbol** `type` on the server was successful.
+(define (a-load db type path)
+  (assert (and (symbol? type) (string? path) (string-ci=? "json" (pathname-extension path))))
+  (and (a-command db 'sql (string-append "CREATE DOCUMENT TYPE " (symbol->string type) " IF NOT EXISTS"))
+       (a-command db 'sql (string-append "INSERT INTO " (symbol->string type) " CONTENT " (read-string #f (open-input-file path))))
+       #t))
+
+;;@returns: **boolean** that is true if backing-up database **symbol** `db` on the server was successful.
+(define (a-backup db)
+  (let [(res (a-command db 'sql "BACKUP DATABASE"))]
+    (and res (not (null? res)) (ok? (car res)))))
+
+;;@returns: **list**-of-**alist**s integrity check report, and attempts to fix if true **boolean** `fix?` is passed.
+(define (a-check db . fix?)
+  (a-command db 'sql (string-append "CHECK DATABASE" (if (optional fix? #f) " FIX" ""))))
+
+;;@returns: **string** comment for database **symbol** `db`, or `#t` if **string** `msg` is passed.
+(define (a-comment db . msg)
+  (assert (symbol? db))  
+  (and (a-command db 'sql (string-append "CREATE DOCUMENT TYPE D IF NOT EXISTS"))
+       (if (null? msg) (let [(res (a-query db 'sql (string-append "SELECT comment FROM D WHERE on = \"database\" LIMIT 1")))]
+                         (and res
+                              (not (null? res))
+                              (not (null? (car res)))
+                              (alist-ref 'comment (car res))))
+                       (let [(str (car msg))]
+                         (and (string? str)
+                              (a-command db 'sql (string-append "UPDATE D SET comment = \"" str "\" UPSERT WHERE on = \"database\""))
+                              #t))))) 
+
+)
